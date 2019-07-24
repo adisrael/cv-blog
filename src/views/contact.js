49704e120015e837925f3c728b2f5e5b1b6d8514
Alexander Israel <adisrael@uc.cl>
2019-07-23 23:08:32 -0400

    contact start and noscript update

--- src/views/contact.js
+++ src/views/contact.js
@@ -4,6 +4,18 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
+        <a href="https://github.com/adisrael">GitHub</a>
+        {this.props.lang === "EN"?
+          <a href="https://www.linkedin.com/in/alexander-israel/?locale=en_US">LinkedIn</a>
+        :
+          <a href="https://www.linkedin.com/in/alexander-israel/?locale=es_ES">LinkedIn</a>
+        }
+        <a href="mailto:alexanderisraeln@gmail.com">alexanderisraeln@gmail.com</a>
+        <a href="mailto:adisrael@uc.cl">adisrael@uc.cl</a>
+        <a href="https://telegram.me/adisrael">Telegram</a>
+        {/* <a href="mailto:adisrael@uc.cl?subject=The%20subject%20of%20the%20mail">adisrael@uc.cl</a> */}
+        <a href="assets/CV_alexander_israel_ES.pdf" download>CV Español</a>
+        
       </div>
     );
   }
--- src/views/home.js
+++ src/views/home.js
@@ -62,7 +62,7 @@ class Home extends Component {
     <div>
       <Title title="ALEXANDER ISRAEL NAVON"/>
 
-      <Contact />
+      <Contact lang={this.state.lang}/>
 
       <NavBar current={this.state.categoryTitle} onChange={this.onChange} lang={this.state.lang}/>
 
