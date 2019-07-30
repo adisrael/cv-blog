ed5cb7f2e6486007c3dce12faa10ceeed833f9db
Alexander Israel <adisrael@uc.cl>
2019-07-30 14:25:43 -0400

    contact updated

--- src/views/contact.js
+++ src/views/contact.js
@@ -3,18 +3,28 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div className="contact">
-        <a href="https://github.com/adisrael">GitHub</a>
+      <div className="contact-container">
+
+        <a className="contact" href="https://github.com/adisrael">github</a>
+
         {this.props.lang === "EN"?
-          <a href="https://www.linkedin.com/in/alexander-israel/?locale=en_US">LinkedIn</a>
+          <a className="contact" href="https://www.linkedin.com/in/alexander-israel/?locale=en_US">linkedin</a>
         :
-          <a href="https://www.linkedin.com/in/alexander-israel/?locale=es_ES">LinkedIn</a>
+          <a className="contact" href="https://www.linkedin.com/in/alexander-israel/?locale=es_ES">linkedin</a>
         }
-        <a href="mailto:alexanderisraeln@gmail.com">alexanderisraeln@gmail.com</a>
-        <a href="mailto:adisrael@uc.cl">adisrael@uc.cl</a>
-        <a href="https://telegram.me/adisrael">Telegram</a>
+
+        <a className="contact" href="mailto:alexanderisraeln@gmail.com">email</a>
+
+        {/* <a className="contact" href="mailto:adisrael@uc.cl">adisrael@uc.cl</a> */}
+
+        <a className="contact" href="https://telegram.me/adisrael">telegram</a>
+
         {/* <a href="mailto:adisrael@uc.cl?subject=The%20subject%20of%20the%20mail">adisrael@uc.cl</a> */}
-        <a href="assets/CV_alexander_israel_ES.pdf" download>CV Español</a>
+        {this.props.lang === "EN"?
+          <a className="contact" href="assets/CV_alexander_israel_EN.pdf" download>cv</a>
+        :
+          <a className="contact" href="assets/CV_alexander_israel_ES.pdf" download>cv</a>
+        }
         
       </div>
     );
